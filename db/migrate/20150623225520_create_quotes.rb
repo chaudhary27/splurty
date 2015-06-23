06bcbd8735ea569338d801a001d894de13f09729
Faisal <chaudhary.fsl@gmail.com>
2015-06-23 23:27:41 +0000

    added random function in the controller, also added some quotes in db using rails console

--- db/migrate/20150623225520_create_quotes.rb
+++ db/migrate/20150623225520_create_quotes.rb
@@ -2,10 +2,9 @@ class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
 
-
       t.string :saying
       t.string :author
-      
+
       t.timestamps
     end
   end
